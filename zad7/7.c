4fa10b405a92ff834a157857b58823d920466935
Krzysztof Mańczak <krzysztfmanczak@gmail.com>
2023-10-20 21:45:08 +0200

    Drunk coding is BACK ON THE MENU BOYZZZZ

--- zad7/7.c
+++ zad7/7.c
@@ -5,23 +5,67 @@
 
 #define MAX 1024
 
-int main(int argc, char*argv[]){
+void Chars(int origin, int destination){
     char buf[MAX];
-    char singleBuf;
+    lseek(origin,0,SEEK_END);
+
+    int words=0;
+    while(words<10){
+        lseek(origin,-1,SEEK_CUR);
+        read(origin,&buf[words],1);
+        lseek(origin,-1,SEEK_CUR);
+        if(buf[words]!='\n'){
+            words++;
+        }
+        else{
+            lseek(origin,-1,SEEK_CUR);
+        }
+    }
+    for(int i=9;i>0;i--){
+        printf("%c",buf[i]);
+        write(destination,&buf[i],1);
+    }
+}
+
+void Words(int origin, int destination){
+    char buf[10][MAX];
+    char byte[MAX];
+    lseek(origin,0,SEEK_END);
+
+    int words=0;
+    while(words<10){
+        lseek(origin,-1,SEEK_CUR);
+        read(origin,&byte,1);
+        lseek(origin,-1,SEEK_CUR);
+        if(byte=='\n' || byte==' '){
+            words++;
+        }
+        else{
+            lseek(origin,-1,SEEK_CUR);
+        }
+    }
+    for(int i=9;i>0;i--){
+        printf("%c",buf[i]);
+        write(destination,&buf[i],1);
+    }
+}
+
+int main(int argc, char*argv[]){
         
     int desc_zrod = open(argv[1],O_RDONLY);
     int desc_dest = open(argv[2],O_WRONLY);
 
-    lseek(desc_zrod,0,SEEK_END);
+    switch (*argv[3])
+    {
+    case 49:
+        Chars(desc_zrod,desc_dest);
+        break;
+    case 50:
 
-    int words=0;
-    for(int i =0;i<12;i++){
-        lseek(desc_zrod,-1,SEEK_CUR);
-        read(desc_zrod,&singleBuf,1);
-        lseek(desc_zrod,-1,SEEK_CUR);
-        printf("%c \n",singleBuf);
+        break;
+    default:
+        break;
     }
-
 
     close(desc_dest);
     close(desc_zrod);
