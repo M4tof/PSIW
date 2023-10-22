6660a5db7b6abe59cbbde35e07e755532130fb31
Krzysztof Mańczak <krzysztfmanczak@gmail.com>
2023-10-22 16:38:15 +0200

    15

--- zadd15/15.c
+++ zadd15/15.c
@@ -6,17 +6,52 @@
 
 #define MAX 1024
 
-void searchForChain(int desc, char *chain, const char *filename) {
+void searchForChain(const char *chain, const char *filename) {
+    int line = 1;
+    int chainstart = 0;
+    int origin = open(filename, O_RDWR);
 
+    char buf;
+    int correct = 1;
+    int start = 0;
+    char currentChainChar;
+
+    while (read(origin, &buf, 1) > 0) {
+        chainstart++;
+        if (buf == '\n') {
+            line++;
+            chainstart = 0;
+        }
+
+        if (correct && buf == chain[start]) {
+            if (start == 0) {
+                currentChainChar = buf;
+            }
+
+            start++;
+
+            if (start == strlen(chain)) {
+                printf("Chain '%s' was found in file %s on line %d beginning at character %d\n", chain, filename, line, chainstart - strlen(chain)+1);
+                start = 0;
+                correct = 1;
+            }
+        } 
+        
+        else {
+            start = 0;
+            correct = 1;
+        }
+    }
+
+    close(origin);
 }
 
 int main(int argc, char *argv[]) {
-    char *searchChain = argv[1];
+    const char *searchChain = argv[1];
 
     for (int i = 2; i < argc; i++) {
-        int origin = open(argv[i], O_RDWR);
-        searchForChain(origin, searchChain, argv[i]);
-        close(origin);
+        searchForChain(searchChain, argv[i]);
     }
+
     return 0;
 }
